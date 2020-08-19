77d82ec25235bf3562d0f3b2a534a7f90056f92d
CoreyWhitmore <cjwhit97@hotmail.com>
2020-08-19 14:07:03 -0600

    Initial Commit

---
+++ app.js
@@ -0,0 +1,65 @@
+let player = ''
+let choices = [
+    {
+        name: 'Rock',
+        beats: ['Scissors', 'Lizard'],
+        loses: ['Paper', 'Spock']
+    },
+    {
+        name: 'Paper',
+        beats: ['Rock', 'Spock'],
+        loses: ['Scissors', 'Lizard']
+    },
+    {
+        name: 'Scissors',
+        beats: ['Paper', 'Lizard'],
+        loses: ['Rock', 'Spock']
+    },
+    {
+        name: 'Lizard',
+        beats: ['Paper', 'Spock'],
+        loses: ['Rock', 'Scissors']
+    },
+    {
+        name: 'Spock',
+        beats: ['Rock', 'Scissors'],
+        loses: ['Paper', 'Lizard']
+    }
+]
+
+
+function select(selection) {
+    player = selection
+    document.getElementById('Results').innerText = `You have selected: ${selection.name}`
+}
+
+function play(selection) {
+
+    document.getElementById('Results').innerText = 'The Opponent is choosing...'
+
+
+    let computerSelect = choices[(Math.floor(Math.random() * choices.length - 0.001))]
+    let message = `You Picked: ${selection.name}, your opponent picked: ${computerSelect.name}`
+    if (selection.beats.find(pick => pick == computerSelect.name)) {
+        message += " - You Win!"
+    } else if (selection.loses.find(pick => pick == computerSelect.name)) {
+        message += " - You Lose :("
+    } else {
+        message += " - You Tied -.-"
+    }
+
+
+    document.getElementById('Results').innerText = message
+
+}
+
+function drawButtons() {
+    let template = ''
+    for (let i = 0; i < choices.length; i++) {
+        const choice = choices[i];
+        template += `<button type="button" class="btn btn-primary option m-1" onclick="select(choices[${i}])">${choice.name}</button>\n`
+    }
+    document.getElementById('selection-menu').innerHTML = template
+}
+
+drawButtons()
